f8d3df378ebb25e28ee203d297cf739a6736313d
reza ul <rezaul.bd77@gmail.com>
2021-03-20 13:11:39 +0600

    Add project files.

---
+++ BlazorCrud/Server/Migrations/20210320061655_v1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorCrud.Server.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(unicode: false, maxLength: 100, nullable: false),
+                    Designation = table.Column<string>(unicode: false, maxLength: 100, nullable: true),
+                    Email = table.Column<string>(unicode: false, maxLength: 20, nullable: true),
+                    Location = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
